28877581bc12b950eb1913ee842e01e81bfb6935
gisel31 <maria.sellanes@estudiantes.utec.edu.uy>
2023-06-02 02:01:02 -0300

    Merge branch 'master' of https://github.com/UlisesCasero/lab_ria

--- src/app/alta-areas/alta-areas.component.ts
+++ src/app/alta-areas/alta-areas.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-alta-areas',
@@ -8,22 +9,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AltaAreasComponent {
   public id: number = 0;
-  public nombre: String = "";
-  public activo: boolean = true;
-  public error: String = '';
+  public nombre: string = "";
+  public activo: boolean = false;
+  public error: string = '';
 
   constructor(private http: HttpClient) { }
 
-  altaAreas(nombre: String) {
+  altaAreas(nombre: string, activo: boolean) {
+    if (!nombre) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El nombre del área no puede estar vacío',
+        timer: 2000,
+        timerProgressBar: true
+      });
+      return;
+    }
+
     const url = `http://localhost:5000/api/Areas`;
-    const nom = { nombre: nombre };
-    this.http.post<any>(url, nom).subscribe(
+    const area = {
+      nombre: nombre,
+      activo: activo
+    };
+
+    this.http.post<any>(url, area).subscribe(
       (response) => {
-        console.log('Area creada:', response);
+        console.log('Área creada:', response);
+        Swal.fire({
+          icon: 'success',
+          title: 'Éxito',
+          text: 'El área se guardó correctamente',
+          timer: 2000,
+          timerProgressBar: true
+        });
       },
       (error) => {
-        console.log('Error al crear el area:', error);
-        this.error = 'Error al crear el area';
+        console.log('Error al crear el área:', error);
+        this.error = 'Error al crear el área';
       }
     );
   }
