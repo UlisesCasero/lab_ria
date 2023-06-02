723489c1575440c77d7be80ff2bb6cf9470613d2
UlisesCasero <ulisesd.casero@gmail.com>
2023-06-02 12:07:06 -0300

    listar documentos

--- src/app/listar-tipo-documentos/listar-tipo-documentos.component.ts
+++ src/app/listar-tipo-documentos/listar-tipo-documentos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-listar-tipo-documentos',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-tipo-documentos.component.scss']
 })
 export class ListarTipoDocumentosComponent {
+  Documentos: any[] = [];
+  public error: String = '';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.obtenerTipoDocumentos();
+  }
+
+  obtenerTipoDocumentos() {
+    const url = 'http://localhost:5000/api/TiposDeDocumentos/Paged';
+    const Body = {
+      limit: -1,
+      offset: 0,
+      id: 0,
+      filters: {
+        activo: true,
+        nombre: ''
+      },
+      orders: ['']
+    };
+
+    this.http.post<any>(url, Body).subscribe(
+      (response) => {       
+        console.log('Documentos:', response);   
+        this.Documentos = response.list;   
+      },
+      (error) => {
+        console.log('Error al obtener los documents');
+        this.error = `Error al obtener los documents`;
+      } 
+    );
+  }
 }
